e652de0076c775126ed79a256980edd317fb924b
harsha <mchadraharsha04@gmail.com>
2024-06-04 11:07:37 +0530

    added statements

--- loose_tight_coupling_JVM/src/main/java/coupling/loose_tight_coupling_JVM/AppGamingbasic2.java
+++ loose_tight_coupling_JVM/src/main/java/coupling/loose_tight_coupling_JVM/AppGamingbasic2.java
@@ -1,5 +1,5 @@
 package coupling.loose_tight_coupling_JVM;
-
+import coupling.loose_tight_coupling_JVM.loosegame.GameRunner;
 import coupling.loose_tight_coupling_JVM.loosegame.GameRunner;
 import coupling.loose_tight_coupling_JVM.loosegame.SuperContragame;
 import coupling.loose_tight_coupling_JVM.loosegame.MarioGame;
@@ -9,7 +9,7 @@ import coupling.loose_tight_coupling_JVM.loosegame.MarioGame;
 
 public class AppGamingbasic2 {
     public static void main(String args[]){
-        var game =  new MarioGame();    //object creation
+        var game = new MarioGame();    //object creation
         var gamerunner = new GameRunner(game); //object creation + wiring dependencies
         //Game is dependency of GameRunner
         gamerunner.run();
