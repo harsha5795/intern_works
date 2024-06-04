5098efe92303f7de74e10ff398493b8e3c45bac7
harsha <mchadraharsha04@gmail.com>
2024-06-04 11:05:51 +0530

    added statements

--- loose_tight_coupling_JVM/src/main/java/coupling/loose_tight_coupling_JVM/AppGamingbasic2.java
+++ loose_tight_coupling_JVM/src/main/java/coupling/loose_tight_coupling_JVM/AppGamingbasic2.java
@@ -4,9 +4,12 @@ import coupling.loose_tight_coupling_JVM.loosegame.GameRunner;
 import coupling.loose_tight_coupling_JVM.loosegame.SuperContragame;
 import coupling.loose_tight_coupling_JVM.loosegame.MarioGame;
 
+//Here Gamerunner dont interact directly with the Games, instaed it interacts with the gaming console interface
+//and Gaming console interface can be any game according to the parameter sent.
+
 public class AppGamingbasic2 {
     public static void main(String args[]){
-        var game = new MarioGame();    //object creation
+        var game =  new MarioGame();    //object creation
         var gamerunner = new GameRunner(game); //object creation + wiring dependencies
         //Game is dependency of GameRunner
         gamerunner.run();
