4615174bfb9b94880b89652211d588d0876dc598
harsha <mchadraharsha04@gmail.com>
2024-06-04 19:21:41 +0530

    added statements

--- loose_tight_coupling_JVM/src/main/java/coupling/loose_tight_coupling_JVM/LooseTightCouplingJvmApplication.java
+++ loose_tight_coupling_JVM/src/main/java/coupling/loose_tight_coupling_JVM/LooseTightCouplingJvmApplication.java
@@ -10,6 +10,7 @@ public class LooseTightCouplingJvmApplication {
 		SpringApplication.run(LooseTightCouplingJvmApplication.class, args);
 		System.out.println("Hello modified");
 		System.out.println("from the ram branch");
+		System.out.println("hey harsha");
 	}
 
 }
